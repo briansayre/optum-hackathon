5ab9063724870acdd463d6e15c1393a31b062714
Brian Sayre <briansayre01@gmail.com>
2021-08-06 08:37:15 -0500

    Added more feedback to user

--- app.py
+++ app.py
@@ -1,31 +1,99 @@
-from flask import Flask, render_template, Response
-import cv2 as cv
+from flask import Flask, request, render_template, Response
+import cv2
 import numpy as np
+import mediapipe as mp
+import time
+from collections import Counter
+import pandas as pd
 
 app = Flask(__name__)
 
-camera = cv.VideoCapture(0)  # use 0 for web camera
+camera = cv2.VideoCapture(0)  # use 0 for web camera
 #  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
-# for local webcam use cv.VideoCapture(0)
+# for local webcam use cv2.VideoCapture(0)
+
+
+
+def get_thumb_status(wy, ty):
+    differential = wy - ty
+    if differential <= .1 and differential >= -.1:
+        return 0
+    elif differential > .1:
+        return 1
+    elif differential < -.1:
+        return -1
+
 
 def gen_frames():  # generate frame by frame from camera
+    
+    counter = 0
+    thumb_status = 0
+    last_status = 0
+    symptom_list = []
+    cap = cv2.VideoCapture(0)
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
+
     while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
+        success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+
+
+        wrist_y = 0
+        thumb_y = 0
+        
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                for id, lm in enumerate(handLms.landmark):
+                    # print(id,lm)
+
+                    if id == 0:
+                        wrist_y = lm.y
+                    if id == 4:
+                        thumb_y = lm.y
+                    
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x *w), int(lm.y*h)
+
+                    cv2.circle(img, (cx,cy), 7, (255,0,255), cv2.FILLED)
+
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+
+        last_status = thumb_status
+        thumb_status = get_thumb_status(wrist_y, thumb_y)
+
+        if last_status == thumb_status and thumb_status != 0:
+            counter += 1
         else:
-            hsvim = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-            lower = np.array([0, 48, 80], dtype = "uint8")
-            upper = np.array([20, 255, 255], dtype = "uint8")
-            skinRegionHSV = cv.inRange(hsvim, lower, upper)
-            blurred = cv.blur(skinRegionHSV, (2,2))
-            ret,thresh = cv.threshold(blurred,0,255,cv.THRESH_BINARY)
-            cv.imshow("thresh", thresh)
-            ret, buffer = cv.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+            counter = 0
+
+        if counter >= 50:
+            counter = 50
+
+        # cv2.putText(img,"Thumb status: " + str(thumb_status), (10,15), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1)
+        # cv2.putText(img,"Counter: " + str(counter), (10,30), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1)
+
+        overlay = img.copy()
+        center_coordinates = (75, 75)
+        radius = 50
+        color = (255, 255, 255)
+        if thumb_status == 1:
+            color = (20, 255, 20, .5)
+        elif thumb_status == -1:
+            color = (20, 20, 255)
+        thickness = 2
+        cv2.circle(overlay, center_coordinates, radius+2, (230,230,230), thickness)
+        cv2.circle(overlay, center_coordinates, counter, color, -1)
+        alpha = 0.4  
+        img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
+
+        ret, buffer = cv2.imencode('.jpg', img)
+        img = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')  # concat frame one by one and show result
 
 
 @app.route('/video_feed')
@@ -39,6 +107,17 @@ def index():
     """Video streaming home page."""
     return render_template('index.html')
 
+
+@app.route('/form')
+def form():
+    """Video streaming home page."""
+    return render_template('form.html')
+
+@app.route('/response', methods=['POST'])
+def response():
+    fname = request.form.get("fname")
+    note = request.form.get("note")
+    return render_template("index.html", name=fname, note=note)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- hand_tracking.py
+++ hand_tracking.py
@@ -1,6 +1,10 @@
 import cv2
 import mediapipe as mp
 import time
+from collections import Counter
+import pandas as pd
+
+symptom_list = []
 
 cap = cv2.VideoCapture(0)
 
@@ -10,6 +14,36 @@ mpDraw = mp.solutions.drawing_utils
 
 pTime = 0
 cTime = 0
+
+def findCommon(list1, list2):
+    return list(set(list1).intersection(list2))
+
+
+def get_thumb_status(wy, ty):
+    differential = wy - ty
+    if differential <= .1 and differential >= -.1:
+        return 0
+    elif differential > .1:
+        return 1
+    elif differential < -.1:
+        return -1
+
+df  = pd.read_csv('data/dataset.csv')
+empty = []
+
+for i in range(1, 17): 
+    empty += df['Symptom_' + str(i)].tolist()
+
+empty = {k: v for k, v in sorted(Counter(empty).items(), key=lambda item: item[1], reverse=True)}
+del empty[list(empty.keys())[0]]
+# [print(k,':',v) for k, v in empty.items()]
+
+for key in empty:
+    print(key)
+
+counter = 0
+thumb_status = 0
+last_status = 0
 
 while True:
     success, img = cap.read()
@@ -44,16 +78,20 @@ while True:
 
     cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 
-    thumb_status = "unknown"
-    if wrist_y < thumb_y:
-        thumb_status = "Thumb down"
-    elif wrist_y > thumb_y:
-        thumb_status = "Thumb up"
+    last_status = thumb_status
+    thumb_status = get_thumb_status(wrist_y, thumb_y)
 
-    cv2.putText(img,thumb_status, (10,140), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    if last_status == thumb_status and thumb_status != 0:
+        counter += 1
+    else:
+        counter = 0
+
+    cv2.putText(img,str(thumb_status), (10,140), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 
     # cv2.putText(img,str(wrist_y), (10,210), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
     # cv2.putText(img,str(thumb_y), (10,280), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    cv2.putText(img,str(counter), (10,280), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+
